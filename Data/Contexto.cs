d4a1dc98d8cd053168f8b58dac1a6c3858ab4f85
GabiDourado <gabi.dourados2xd@gmai.com>
2024-09-11 10:44:07 -0300

    Criação do Cotexto e usuarios map

---
+++ Data/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using ComuniQApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComuniQApi.Data
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+
+        public DbSet<UsuariosModel> Usuario { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuariosMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Data/UsuariosMap.cs
@@ -0,0 +1,26 @@
+﻿using ComuniQApi.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComuniQApi.Data
+{
+    public class UsuariosMap : IEntityTypeConfiguration<UsuariosModel>
+    {
+        public void Configure(EntityTypeBuilder<UsuariosModel> builder)
+        {
+            builder.HasKey(x => x.UsuarioId);
+            builder.Property(x => x.UsuarioNome).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.UsuarioSobrenome).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.UsuarioApelido).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.UsuarioEmail).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.UsuarioTelefone).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.UsuarioCPF).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.UsuarioCEP).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.UsuarioCidade).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.UsuarioBairro).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.UsuarioEstado).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.UsuarioSenha).IsRequired().HasMaxLength(255);
+        }
+    }
+}
+
