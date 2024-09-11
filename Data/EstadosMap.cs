fcc5635cb1e3d45916a97abfa6bb7af91a19e41d
GabiDourado <gabi.dourados2xd@gmai.com>
2024-09-11 11:54:50 -0300

    Ajustes - Estado

---
+++ Data/EstadosMap.cs
@@ -0,0 +1,15 @@
+﻿using ComuniQApi.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComuniQApi.Data
+{
+    public class EstadosMap : IEntityTypeConfiguration<EstadosModel>
+    {
+        public void Configure(EntityTypeBuilder<EstadosModel> builder)
+        {
+            builder.HasKey(x => x.EstadoId);
+            builder.Property(x => x.EstadoNome).IsRequired().HasMaxLength(255);
+        }
+    }
+}
---
+++ Repositorios/Interfaces/IEstadosRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using ComuniQApi.Models;
+
+namespace ComuniQApi.Repositorios.Interfaces
+{
+    public interface IEstadosRepositorio
+    {
+        Task<List<EstadosModel>> GetAll();
+
+        Task<EstadosModel> GetById(int id);
+
+        Task<EstadosModel> InsertEstado(EstadosModel estado);
+
+        Task<EstadosModel> UpdateEstado(EstadosModel estado, int id);
+
+        Task<bool> DeleteEstado(int id);
+    }
+}
