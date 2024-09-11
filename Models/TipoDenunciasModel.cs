cee8b5953b00e939f0aa1bfe9b3e7b36b7acd8ae
GabiDourado <gabi.dourados2xd@gmai.com>
2024-09-11 09:36:38 -0300

    Base models

---
+++ Models/TipoDenunciasModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ComuniQApi.Models
+{
+    public class TipoDenunciasModel
+    {
+        public int TipoDenunciaId { get; set; }
+
+        public string TipoDenunciaNome { get; set; } = string.Empty;
+
+        public static implicit operator List<object>(TipoDenunciasModel v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
