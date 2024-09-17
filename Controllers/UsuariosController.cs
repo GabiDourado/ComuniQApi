94323075fb1fccd1149cac12521f82e28680a710
GabiDourado <gabi.dourados2xd@gmai.com>
2024-09-17 11:44:59 -0300

    usuarios controller

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,57 @@
+﻿using ComuniQApi.Models;
+using ComuniQApi.Repositorios.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComuniQApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly IUsuariosRepositorio _usuariosRepositorio;
+
+        public UsuariosController(IUsuariosRepositorio usuariosRepositorio)
+        {
+            _usuariosRepositorio = usuariosRepositorio;
+        }
+        [HttpGet("GetAllUsuarios")]
+        public async Task<ActionResult<List<UsuariosModel>>> GetAllUsuarios()
+        {
+            List<UsuariosModel> usuarios = await _usuariosRepositorio.GetAll();
+            return Ok(usuarios);
+        }
+        [HttpGet("GetUsuarioId/{id}")]
+        public async Task<ActionResult<UsuariosModel>> GetUsuarioId(int id)
+        {
+            UsuariosModel usuario = await _usuariosRepositorio.GetById(id);
+            return Ok(usuario);
+        }
+        [HttpPost("InsertUsuario")]
+        public async Task<ActionResult<UsuariosModel>> InsertUsuario([FromBody] UsuariosModel usuarioModel)
+        {
+            UsuariosModel usuario = await _usuariosRepositorio.InsertUsuario(usuarioModel);
+            return Ok(usuario);
+        }
+        [HttpPost("Login")]
+        public async Task<ActionResult<bool>> Login([FromBody] UsuariosModel usuarioModel)
+        {
+            var usuario = await _usuariosRepositorio.Login(usuarioModel.UsuarioEmail, usuarioModel.UsuarioSenha);
+            return Ok(usuario);
+
+        }
+
+        [HttpPut("UpdateUsuario/{id:int}")]
+        public async Task<ActionResult<UsuariosModel>> UpdateUsuario(int id, [FromBody] UsuariosModel usuarioModel)
+        {
+            usuarioModel.UsuarioId = id;
+            UsuariosModel usuario = await _usuariosRepositorio.UpdateUsuario(usuarioModel, id);
+            return Ok(usuario);
+        }
+        [HttpDelete("DeleteUsuario/{id:int}")]
+        public async Task<ActionResult<UsuariosModel>> DeleteUsuario(int id)
+        {
+            bool deleted = await _usuariosRepositorio.DeleteUsuario(id);
+            return Ok(deleted);
+        }
+    }
+}
